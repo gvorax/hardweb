1ff2c68b45f815a4863b9b79712a15fdbd2047c1
gvorax <otabek83019@gmail.com>
2024-01-27 16:00:09 +0500

    ddd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "@/styles/Header.module.scss";
+import styles from "../styles/Header.module.scss";
 import Navbar from "./Navbar";
 import video from '../assets/video.mp4';
 export default function Header() {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Header from '@/components/Header'
-import About from '@/components/About'
-import Promotion from '@/components/Promotion'
+import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
+import About from '../components/About'
+import Promotion from '../components/Promotion'
 
 
 const inter = Inter({ subsets: ['latin'] })

a5b2c899211ba0037a0b835c99dd4a1f0eea14ec
gvorax <otabek83019@gmail.com>
2024-02-22 13:16:52 +0500

    added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Header from "../components/Header";
 import About from "../components/About";
 import Promotion from "../components/Promotion";
+import '../../googlefb28a543ad928dc7 (1).html'
 
 const inter = Inter({ subsets: ["latin"] });
 
